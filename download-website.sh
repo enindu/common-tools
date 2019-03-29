0ff86ff1c4783dffcfefbad95d1553712b6ff1f6
Enindu Alahapperuma <enindu@gmail.com>
2019-03-29 17:47:15 +0530

    Fix some typos

--- download-website.sh
+++ download-website.sh
@@ -2,13 +2,13 @@
 
 # Original source - https://www.linuxjournal.com/content/downloading-entire-web-site-wget
 
-# Check if website is not specified
+# Check if website and domain is not specified
 if [[ -z $@ ]]; then
     echo "You should specified website and domain";
     exit;
 fi
 
-# Get website
+# Get website and domain
 WEBSITE=$1;
 DOMAIN=$2;
 
